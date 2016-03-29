4a05c7d8845b52bb902f376f1d6a56154f6e3a81
Gilles Vanpeteghem <gilles_vanpeteghem@hotmail.com>
2016-03-29 12:42:22 +0200

    Eight Commit - Changed the create.blade view (changed type_created to date format, changed url of Form::open)

--- resources/views/Types/create.blade.php
+++ resources/views/Types/create.blade.php
@@ -1,9 +1,11 @@
-{{ Form::open(array('url' => 'types/create', 'route' => 'types/store')) }}
+<p>CREATE A TYPE</p>
+
+{{ Form::open(array('url' => 'types')) }}
 	{{ Form::label('type_title', 'Title')}}
     {{ Form::text('type_title') }}
     {{ Form::label('type_description', 'Description')}}
     {{ Form::text('type_description') }}
     {{ Form::label('type_created', 'Created')}}
-    {{ Form::text('type_created') }}
+    {{ Form::date('type_created') }}
     {{ Form::submit('Create a Type!') }}
 {{ Form::close() }}
