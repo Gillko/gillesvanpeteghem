8aa2113350d0c3d4c4156be173f43a8b5aa0a286
Gilko <gilles_vanpeteghem@hotmail.com>
2016-06-25 17:54:51 +0200

    24th commit - Homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,64 +3,188 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Laravel</title>
+        <title>Gilles Vanpeteghem</title>
+
+        <link rel="shortcut icon" href="../resources/assets/img/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="../resources/assets/img/favicon.ico" type="image/x-icon">
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-        <link href="../resources/assets/css/app.css" rel="stylesheet" type="text/css">
-
         <link href="../resources/assets/css/libraries/foundation/foundation.min.css" rel="stylesheet" type="text/css">
 
-        <style>
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-            .title {
-                font-family: 'Lato';
-                font-size: 96px;
-            }
-        </style>
+        <link href="../resources/assets/css/app.css" rel="stylesheet" type="text/css">
+
+        <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
     </head>
     <body>
         <!-- Responsive navigation -->
-        <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
-            <button class="menu-icon" type="button" data-toggle></button>
-            <div class="title-bar-title">Gilles Vanpeteghem</div>
+        <div class="title-bar title-bar-center title-bar-background" data-responsive-toggle="example-menu" data-hide-for="medium">
+            <div class="title-bar-title">
+                <a href="/">
+                    <img src="../resources/assets/img/gilles_vanpeteghem_logo.png" class="logo">
+                </a>
+            </div>
+            <button class="menu-icon menu-icon-color" type="button" data-toggle></button>
         </div>
       
-        <div class="top-bar" id="example-menu">
-            <div class="top-bar-left">
-                <ul class="dropdown menu" data-dropdown-menu>
-                    <li class="menu-text">GV</li>
+        <div class="top-bar top-bar-center top-bar-background" id="example-menu">
+            <div class="top-bar-inner">
+                <ul class="dropdown menu menu-bottom" data-dropdown-menu>
                     <li>
-                        <a href="#">Who</a>
-                        <ul class="menu vertical">
+                        <a href="#who">who</a>
+                        <!-- <ul class="menu vertical">
                             <li><a href="#">Who First</a></li>
                             <li><a href="#">Who Second</a></li>
                             <li><a href="#">Who Third</a></li>
-                        </ul>
+                        </ul> -->
                     </li>
-                    <li><a href="#">What</a></li>
-                    <li><a href="#">Why</a></li>
-                    <li><a href="#">When</a></li>
-                </ul>
-            </div>
-            <div class="top-bar-right">
-                <ul class="menu">
-                    <li><input type="search" placeholder="Search"></li>
-                    <li><button type="button" class="button">Search</button></li>
+                    <li>
+                        <a href="#what">what</a>
+                    </li>
+                    <li class="menu-text">
+                        <a href="/">
+                            <img src="../resources/assets/img/gilles_vanpeteghem_logo.png" class="logo">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#why">why</a>
+                    </li>
+                    <li>
+                        <a href="#when">when</a>
+                    </li>
                 </ul>
             </div>
         </div>
         <div class="container">
             <div class="content">
-                <!-- <div class="title small-4 columns">Laravel Gilles Vanpeteghem</div> -->
-                <div class="title">Laravel Gilles Vanpeteghem</div>
+                <div class="blueBox colorWhite">
+                    <div class="row">
+                        <div class="title large-12 column">
+                            <h1 id="who">Gilles Vanpeteghem</h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="large-4 medium-3 small-1 columns">
+                            <div class="line"></div>
+                        </div>
+                        <div class="large-4 medium-6 small-10 columns portrait">
+                            <img src="../resources/assets/img/gilles_vanpeteghem.jpg" alt="Gilles Vanpeteghem">
+                        </div>
+                        <div class="large-4 medium-3 small-1 columns">
+                            <div class="line"></div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="large-6 medium-6 small-12 columns">
+                            <p>
+                                Welcome on my one page website. As you can see I love simplicity! My name is Gilles Vanpeteghem and I'm a 26 year old web developer from Ghent. I am the last born <i class="em em-baby_bottle"></i> in a family <i class="em em-family"></i> of 3 children. Born and raised, you could have guessed, in Ghent. Only the best city in the world, just kidding. But it's a great city. <i class="em em-city_sunrise"></i>
+                            </p>
+                            <p>
+                                In my spare time I play lots of snooker and/or pool. <i class="em em-8ball"></i> I'm kinda obsessed by the game. In the past I've played competition pool and in September I will be playing compitition snooker for the first time. Next to snooker I like to play a video game once in a while. <i class="em em-video_game"></i> Also I'd like to go out with friends sometime to have a beer. <i class="em em-beer"></i> Or on some occasions to have some beers. <i class="em em-beers"></i> <!-- <i class="em em-stuck_out_tongue"></i> -->
+                            </p>
+                        </div>
+                        <div class="large-6 medium-6 small-12 columns">
+                            <p>
+                                I have a great interest in everything multimedia related. <i class="em em-iphone"></i> I'm online everyday. <i class="em em-computer"></i> Lorem ipsum dolor sit amet, ad ullum inermis nostrum per. Nam quidam aliquando democritum et, id appareat intellegebat ius. Cu sea augue aeque delicatissimi, ei alia sapientem eam. Ea erant rationibus mel. Vel ex error graece, aperiam ancillae mei ex. Elitr homero vidisse mea id, tota viderer philosophia per ne.
+                            </p>
+                            <p>
+                                Eros menandri deseruisse sed ne, esse consetetur appellantur sit eu. Eu mundi dicam scripta eam, ex brute corrumpit omittantur per. Ius ea tantas prodesset, enim praesent te vis. Eu facilis oportere repudiandae mea, no harum suavitate per. In quando detracto prodesset vel, no usu dicant verear.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="whiteBox colorBlue">
+                    <div class="row">
+                        <div class="title large-12 column">
+                            <h1 id="what">What</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="blueBox colorWhite">
+                    <div class="row">
+                        <div class="title large-12 column">
+                            <h1 id="why">Why</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="whiteBox colorBlue">
+                    <div class="row">
+                       <div class="title large-12 column">
+                            <h1 id="when">When</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <footer>
+                <div class="row">
+                    <!-- <div class="large-4 push-8 columns">
+                        <ul class="home-social">
+                            <li>
+                                <a href="http://www.twitter.com/ZURB" class="twitter"></a>
+                            </li>
+                            <li>
+                                <a href="http://www.facebook.com/ZURB" class="facebook"></a>
+                            </li>
+                            <li>
+                                <a href="http://zurb.com/contact" class="mail"></a>
+                            </li>
+                        </ul>
+                    </div> -->
+                    <div class="large-8 medium-8 small-12 columns">
+                        <!-- <a href="http://zurb.com" class="zurb-logo regular"></a>
+                        <ul class="zurb-links">
+                            <li>
+                                <a href="http://zurb.com/about">About</a>
+                            </li>
+                            <li>
+                                <a href="http://zurb.com/blog">Blog</a>
+                            </li>
+                            <li>
+                                <a href="http://zurb.com/news">News<span class="show-for-medium-up"> &amp; Events</span></a>
+                            </li>
+                            <li>
+                                <a href="http://zurb.com/contact">Contact</a>
+                            </li>
+                            <li>
+                                <a href="http://zurb.com/sitemap">Sitemap</a>
+                            </li>
+                        </ul> -->
+                        <p class="text-left">&copy; Copyright 2016 Gilles Vanpeteghem</p>
+                    </div>
+                    <div class="large-4 medium-4 small-12 columns text-right">
+                        <!-- <a href="http://zurb.com" class="zurb-logo regular"></a>
+                        <ul class="zurb-links">
+                            <li>
+                                <a href="http://zurb.com/about">About</a>
+                            </li>
+                            <li>
+                                <a href="http://zurb.com/blog">Blog</a>
+                            </li>
+                            <li>
+                                <a href="http://zurb.com/news">News<span class="show-for-medium-up"> &amp; Events</span></a>
+                            </li>
+                            <li>
+                                <a href="http://zurb.com/contact">Contact</a>
+                            </li>
+                            <li>
+                                <a href="http://zurb.com/sitemap">Sitemap</a>
+                            </li>
+                        </ul> -->
+                        <a href="https://www.facebook.com/g.vanpeteghem" target="_blank">
+                            <img src="../resources/assets/img/facebook.png" class="https://www.facebook.com/g.vanpeteghem">
+                        </a>
+                        <a href="https://twitter.com/Gilkko" target="_blank">
+                              <img src="../resources/assets/img/twitter.png" class="https://twitter.com/Gilkko">
+                        </a>
+                    </div>
+                </div>
+            </footer>
         </div>
         <script src="../resources/assets/js/libraries/jquery/jquery-1.12.1.min.js"></script>
         <script src="../resources/assets/js/libraries/foundation/foundation.min.js"></script>
         <script src="../resources/assets/js/libraries/foundation/app.js"></script>
+
+        <!-- <script src="../resources/assets/js/libraries/jquery/jquery-1.12.1.min.js"></script> -->
+        <script src="../resources/assets/js/app.js"></script>
     </body>
 </html>
