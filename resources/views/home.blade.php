5e29dd38c93a36af42efbf1c5b0ef29012fa140c
Gilles Vanpeteghem <gilles_vanpeteghem@hotmail.com>
2016-08-16 16:25:24 +0200

    64th Commit - changed homepage (new design)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,155 +1,152 @@
 @extends('layouts.layout')
-
 @section('content')
-<div class="container">
-    <div class="content">
-        <div class="backgroundbox">
-            <div class="row">
-                <div class="quote large-5 column">
-                    <h1 id="who">I'm Gilles, a webdeveloper</h1>
-                </div>
-            </div>
-            <div class="row">
-                <div class="quote large-12 column">
-                    <h1 class="actual-quote">They don't make bugs like <br/> bunny anymore.</h1>
-                    <p>Olav Mjelde</p>
-                </div>
-            </div>
-        </div>
-        <div class="grayBox">
-            <div class="row">
-                <div class="title large-3 large-centered column">
-                    <h1 id="expertise">expertise</h1>
-                </div>
-            </div>
-        </div>
-        
-           <!--  <div class="row">
-                <div class="large-12 column">
-                    <p>Eros menandri deseruisse sed ne, esse consetetur appellantur sit eu. Eu mundi dicam scripta eam, ex brute corrumpit omittantur per. Ius ea tantas prodesset, enim praesent te vis. Eu facilis oportere repudiandae mea, no harum suavitate per. In quando detracto prodesset vel, no usu dicant verear. Eros menandri deseruisse sed ne, esse consetetur appellantur sit eu. Eu mundi dicam scripta eam, ex brute corrumpit omittantur per. Ius ea tantas prodesset, enim praesent te vis. Eu facilis oportere repudiandae mea, no harum suavitate per. In quando detracto prodesset vel, no usu dicant verear.</p>
-                </div>
-            </div> -->
-           <!--  <div class="row">
-                <div class="part large-4 column">
-                    <h1 class="part-light">Design</h1>
-                </div>
-                <div class="part large-4 column">
-                   <h1 class="part-dark">Development</h1>
-                </div>
-                <div class="part large-4 column">
-                    <h1 class="part-light">SEO/SEA</h1>
-                </div>
-            </div> -->
-            <div class="row fullWidth-gv">
-                <div class="part-gv large-6 column noPadding-gv grayLightBox">
-                    <div class="row">
-                        <div class="large-6 column large-centered">
-                           <!--  <img src="../resources/assets/img/development.png" class="development" alt=""> -->
-                            <h1 class="development">Development</h1>
-                            <p>Elk project vraagt om een specifieke aanpak. Afhankelijk van wat u precies wenst, hanteer ik steeds de best passende methoden/oplossingen om zo een goede gebruiksvriendelijkheid na te streven en de gewenste doelstellingen te behalen.</p>
-                            <ul>
-                                <li>Frontend &amp; backend</li>
-                                <li>CMS</li>
-                                <li>Responsive websites</li>
-                                <li>Mobile development</li>
-                                <li>E-mail campagnes</li>
-                            </ul>
-                        </div> 
-                    </div>
-                </div>
-                <div class="part-gv large-6 column noPadding-gv grayLighterBox">
-                    <div class="row">
-                        <div class="large-6 column large-centered">
-                           <!--  <img src="../resources/assets/img/hardware.png" class="hardware" alt=""> -->
-                            <h1 class="hardware">Hardware</h1>
-                            <p>Eender welke professional zal beamen dat de juiste/beste apparatuur hanteren het werk ten goede komt. Zo ook als developer. Graag stel ik mijn eigen hardware samen om mijn workflow te optimaliseren of voer ik herstelllingen uit waar nodig.</p>
-                             <ul>
-                                <li>Schermherstellingen (iPhone/iPad)</li>
-                                <li>Samenstellen van PC op maat</li>
-                                <li>Vervangen van batterij (iPhone)</li>
-                                <li>Inwendig onderhoud (bv. verwijderen van stof)</li>
-                                <li>PC/Mac boosten</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    
-        <div class="whiteBox">
-             <div class="row">
-                <div class="title large-3 large-centered column">
-                    <h1 id="skills">skills</h1>
-                </div>
-            </div>
-             <div class="row">
-                <div class="title large-9 large-centered column">
-                    <div class="skills">
-                        <p>HTML5</p>
-                        <p>CSS3</p>
-                        <p>MySQL</p>
-                        <p>PHP</p>
-                        <p>CakePHP</p>
-                        <p>Laravel</p>
-                        <p>jQuery</p>
-                        <p>IIS</p>
-                        <p>After Effects</p>
-                        <p>Final Cut Pro</p>
-                    </div>
-                </div>
-            </div>
-            <!-- <div class="row">
-                <div class="large-12 column">
-                    <p> Eros menandri deseruisse sed ne, esse consetetur appellantur sit eu. Eu mundi dicam scripta eam, ex brute corrumpit omittantur per. Ius ea tantas prodesset, enim praesent te vis. Eu facilis oportere repudiandae mea, no harum suavitate per. In quando detracto prodesset vel, no usu dicant verear. Eros menandri deseruisse sed ne, esse consetetur appellantur sit eu. Eu mundi dicam scripta eam, ex brute corrumpit omittantur per. Ius ea tantas prodesset, enim praesent te vis. Eu facilis oportere repudiandae mea, no harum suavitate per. In quando detracto prodesset vel, no usu dicant verear.</p>
-                </div>
-            </div> -->
-            <!-- <div class="row">
-                <div class="box large-4 medium-4 small-12 column">
-                     <img src="../resources/assets/img/box.png" class="box">
-                </div>
-                <div class="box large-4 medium-4 small-12 column">
-                   <img src="../resources/assets/img/box.png" class="box">
-                </div>
-                <div class="box large-4 medium-4 small-12 column">
-                    <img src="../resources/assets/img/box.png" class="box">
-                </div>
-            </div>
-            <div class="row">
-                <div class="box large-4 medium-4 small-12 column">
-                  <img src="../resources/assets/img/box.png" class="box">
-                </div>
-                <div class="box large-4 medium-4 small-12 column">
-                    <img src="../resources/assets/img/box.png" class="box">
-                </div>
-                <div class="box large-4 medium-4 small-12 column">
-                    <img src="../resources/assets/img/box.png" class="box">
-                </div>
-            </div> -->
-        </div>
-        <div class="blueBox about colorWhite">
-            <div class="row">
-                <div class="title large-2 column">
-                    <h1 id="about">About</h1>
-                </div>
-            </div>
-            <div class="row">
-                <div class="large-5 column">
-                    <p>Ik ben Gilles Vanpeteghem (°1990), een geboren en getogen Gentenaar. Sinds mijn jeugdjaren ben ik uitermate geïnteresseerd in alle computergerelateerde aspecten. In eerste instantie lag de nadruk op gaming en hardware. Later kreeg ik de smaak van het programmeren te pakken en besloot hiervan mijn beroep te maken.</p>
-                    <p>Sinds 2014 mag ik mij Bachelor in de Grafische en Digitale Media noemen, gespecialiseerd in Multimediaproductie ProDEV.</p>
-                    <p>Heeft u een vraag, wenst u mijn portfolio te bekijken of bent u net als ik een fervente pool- en snookerliefhebber? Aarzel dan niet om mij te contacteren via mail of onderstaande kanalen.</p>
-                </div>
-            </div>
-        </div>
-        <div class="socialBox blueDarker">
-            <div class="row">
-                <div class="large-12 medium-12 small-12 column social">
-                    <div class="social-test">
-                        <h1>Let's get social!</h1>
-                        <img src="../resources/assets/img/arrow.png" alt="Gilles Vanpeteghem">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    @include('layouts.footer')
-</div>
+	<div class="container">
+		<div class="content">
+			<div class="backgroundbox">
+				<div class="row">
+					<div class="quote large-5 column">
+						<h1 id="who">I'm Gilles, a webdeveloper</h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="quote large-12 column">
+						<h1 class="actual-quote">They don't make bugs like <br/> bunny anymore.</h1>
+						<p>Olav Mjelde</p>
+					</div>
+				</div>
+			</div>
+			<div class="grayBox">
+				<div class="row">
+					<div class="title large-3 large-centered column">
+						<h1 id="expertise">expertise</h1>
+					</div>
+				</div>
+			</div>
+		   <!--  <div class="row">
+				<div class="large-12 column">
+					<p>Eros menandri deseruisse sed ne, esse consetetur appellantur sit eu. Eu mundi dicam scripta eam, ex brute corrumpit omittantur per. Ius ea tantas prodesset, enim praesent te vis. Eu facilis oportere repudiandae mea, no harum suavitate per. In quando detracto prodesset vel, no usu dicant verear. Eros menandri deseruisse sed ne, esse consetetur appellantur sit eu. Eu mundi dicam scripta eam, ex brute corrumpit omittantur per. Ius ea tantas prodesset, enim praesent te vis. Eu facilis oportere repudiandae mea, no harum suavitate per. In quando detracto prodesset vel, no usu dicant verear.</p>
+				</div>
+			</div> -->
+		   <!--  <div class="row">
+				<div class="part large-4 column">
+					<h1 class="part-light">Design</h1>
+				</div>
+				<div class="part large-4 column">
+				   <h1 class="part-dark">Development</h1>
+				</div>
+				<div class="part large-4 column">
+					<h1 class="part-light">SEO/SEA</h1>
+				</div>
+			</div> -->
+			<div class="row fullWidth-gv">
+				<div class="part-gv large-6 column noPadding-gv grayLightBox">
+					<div class="row">
+						<div class="large-6 column large-centered">
+						   <!--  <img src="../resources/assets/img/development.png" class="development" alt=""> -->
+							<h1 class="development">Development</h1>
+							<p>Elk project vraagt om een specifieke aanpak. Afhankelijk van wat u precies wenst, hanteer ik steeds de best passende methoden/oplossingen om zo een goede gebruiksvriendelijkheid na te streven en de gewenste doelstellingen te behalen.</p>
+							<ul>
+								<li>Frontend &amp; backend</li>
+								<li>CMS</li>
+								<li>Responsive websites</li>
+								<li>Mobile development</li>
+								<li>E-mail campagnes</li>
+							</ul>
+						</div> 
+					</div>
+				</div>
+				<div class="part-gv large-6 column noPadding-gv grayLighterBox">
+					<div class="row">
+						<div class="large-6 column large-centered">
+						   <!--  <img src="../resources/assets/img/hardware.png" class="hardware" alt=""> -->
+							<h1 class="hardware">Hardware</h1>
+							<p>Eender welke professional zal beamen dat de juiste/beste apparatuur hanteren het werk ten goede komt. Zo ook als developer. Graag stel ik mijn eigen hardware samen om mijn workflow te optimaliseren of voer ik herstelllingen uit waar nodig.</p>
+							 <ul>
+								<li>Schermherstellingen (iPhone/iPad)</li>
+								<li>Samenstellen van PC op maat</li>
+								<li>Vervangen van batterij (iPhone)</li>
+								<li>Inwendig onderhoud (bv. verwijderen van stof)</li>
+								<li>PC/Mac boosten</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="whiteBox">
+				 <div class="row">
+					<div class="title large-3 large-centered column">
+						<h1 id="skills">skills</h1>
+					</div>
+				</div>
+				 <div class="row">
+					<div class="title large-9 large-centered column">
+						<div class="skills">
+							<p>HTML5</p>
+							<p>CSS3</p>
+							<p>MySQL</p>
+							<p>PHP</p>
+							<p>CakePHP</p>
+							<p>Laravel</p>
+							<p>jQuery</p>
+							<p>IIS</p>
+							<p>After Effects</p>
+							<p>Final Cut Pro</p>
+						</div>
+					</div>
+				</div>
+				<!-- <div class="row">
+					<div class="large-12 column">
+						<p> Eros menandri deseruisse sed ne, esse consetetur appellantur sit eu. Eu mundi dicam scripta eam, ex brute corrumpit omittantur per. Ius ea tantas prodesset, enim praesent te vis. Eu facilis oportere repudiandae mea, no harum suavitate per. In quando detracto prodesset vel, no usu dicant verear. Eros menandri deseruisse sed ne, esse consetetur appellantur sit eu. Eu mundi dicam scripta eam, ex brute corrumpit omittantur per. Ius ea tantas prodesset, enim praesent te vis. Eu facilis oportere repudiandae mea, no harum suavitate per. In quando detracto prodesset vel, no usu dicant verear.</p>
+					</div>
+				</div> -->
+				<!-- <div class="row">
+					<div class="box large-4 medium-4 small-12 column">
+						 <img src="../resources/assets/img/box.png" class="box">
+					</div>
+					<div class="box large-4 medium-4 small-12 column">
+					   <img src="../resources/assets/img/box.png" class="box">
+					</div>
+					<div class="box large-4 medium-4 small-12 column">
+						<img src="../resources/assets/img/box.png" class="box">
+					</div>
+				</div>
+				<div class="row">
+					<div class="box large-4 medium-4 small-12 column">
+					  <img src="../resources/assets/img/box.png" class="box">
+					</div>
+					<div class="box large-4 medium-4 small-12 column">
+						<img src="../resources/assets/img/box.png" class="box">
+					</div>
+					<div class="box large-4 medium-4 small-12 column">
+						<img src="../resources/assets/img/box.png" class="box">
+					</div>
+				</div> -->
+			</div>
+			<div class="blueBox about colorWhite">
+				<div class="row">
+					<div class="title large-2 column">
+						<h1 id="about">About</h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="large-5 column">
+						<p>Ik ben Gilles Vanpeteghem (°1990), een geboren en getogen Gentenaar. Sinds mijn jeugdjaren ben ik uitermate geïnteresseerd in alle computergerelateerde aspecten. In eerste instantie lag de nadruk op gaming en hardware. Later kreeg ik de smaak van het programmeren te pakken en besloot hiervan mijn beroep te maken.</p>
+						<p>Sinds 2014 mag ik mij Bachelor in de Grafische en Digitale Media noemen, gespecialiseerd in Multimediaproductie ProDEV.</p>
+						<p>Heeft u een vraag, wenst u mijn portfolio te bekijken of bent u net als ik een fervente pool- en snookerliefhebber? Aarzel dan niet om mij te contacteren via mail of onderstaande kanalen.</p>
+					</div>
+				</div>
+			</div>
+			<div class="socialBox blueDarker">
+				<div class="row">
+					<div class="large-12 medium-12 small-12 column social">
+						<div class="social-text">
+							<h1>Let's get social!</h1>
+							<img src="../resources/assets/img/arrow.png" alt="Gilles Vanpeteghem">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		@include('layouts.footer')
+	</div>
 @endsection
