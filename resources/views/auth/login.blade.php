bcba4ef38998befa6a4ff56fe27fc2f51a0430a0
Gilles Vanpeteghem <gilles_vanpeteghem@hotmail.com>
2016-08-16 16:23:35 +0200

    63rd Commit - views/auth/.. changed extends, title, footer section (new design)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,65 +1,68 @@
 @extends('layouts.layout')
-
 @section('content')
-<div class="container">
-    <div class="content">
-        <div class="row">
-            <div class="large-12">
-                <div class="">Login</div>
-                <form class="" role="" method="POST" action="{{ url('/login') }}">
-                    {!! csrf_field() !!}
+	<div class="container">
+		<div class="content">
+			<div class="grayBox">
+				<div class="row">
+					<div class="title large-3 large-centered column">
+						<h1>login</h1>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="large-12">
+					<form class="" role="" method="POST" action="{{ url('/login') }}">
+						{!! csrf_field() !!}
+						<div class="{{ $errors->has('email') ? ' has-error' : '' }}">
+							<label class="">E-Mail Address</label>
 
-                    <div class="{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <label class="">E-Mail Address</label>
+							<div class="">
+								<input type="email" class="" name="email" value="{{ old('email') }}">
 
-                        <div class="">
-                            <input type="email" class="" name="email" value="{{ old('email') }}">
+								@if ($errors->has('email'))
+									<span class="">
+										<strong>{{ $errors->first('email') }}</strong>
+									</span>
+								@endif
+							</div>
+						</div>
+						<div class="{{ $errors->has('password') ? ' has-error' : '' }}">
+							<label class="">Password</label>
 
-                            @if ($errors->has('email'))
-                                <span class="">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                    </div>
+							<div class="">
+								<input type="password" class="" name="password">
 
-                    <div class="{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <label class="">Password</label>
+								@if ($errors->has('password'))
+									<span class="">
+										<strong>{{ $errors->first('password') }}</strong>
+									</span>
+								@endif
+							</div>
+						</div>
+						<div class="">
+							<div class="">
+								<div class="">
+									<label>
+										<input type="checkbox" name="remember"> Remember Me
+									</label>
+								</div>
+							</div>
+						</div>
+						<div class="">
+							<div class="">
+								<button type="submit" class="">
+									<i class=""></i>Login
+								</button>
 
-                        <div class="">
-                            <input type="password" class="" name="password">
-
-                            @if ($errors->has('password'))
-                                <span class="">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="">
-                        <div class="">
-                            <div class="">
-                                <label>
-                                    <input type="checkbox" name="remember"> Remember Me
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="">
-                        <div class="">
-                            <button type="submit" class="">
-                                <i class=""></i>Login
-                            </button>
-
-                            <a class="" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    @include('layouts.footer')
-</div>
+								<a class="" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
+@section('footer')
+	@include('layouts.footer')
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,81 +1,78 @@
 @extends('layouts.layout')
-
 @section('content')
 <div class="container">
-    <div class="content">
-        <div class="row">
-            <div class="large-12">
-                <div class="">Register</div>
-                <form class="" role="form" method="POST" action="{{ url('/register') }}">
-                    {!! csrf_field() !!}
+	<div class="content">
+		<div class="row">
+			<div class="large-12">
+				<div class="">Register</div>
+				<form class="" role="form" method="POST" action="{{ url('/register') }}">
+					{!! csrf_field() !!}
 
-                    <div class="{{ $errors->has('name') ? ' has-error' : '' }}">
-                        <label class="">Name</label>
+					<div class="{{ $errors->has('name') ? ' has-error' : '' }}">
+						<label class="">Name</label>
+						<div class="">
+							<input type="text" class="" name="name" value="{{ old('name') }}">
 
-                        <div class="">
-                            <input type="text" class="" name="name" value="{{ old('name') }}">
+							@if ($errors->has('name'))
+								<span class="">
+									<strong>{{ $errors->first('name') }}</strong>
+								</span>
+							@endif
+						</div>
+					</div>
+					<div class="{{ $errors->has('email') ? ' has-error' : '' }}">
+						<label class="">E-Mail Address</label>
 
-                            @if ($errors->has('name'))
-                                <span class="">
-                                    <strong>{{ $errors->first('name') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                    </div>
+						<div class="">
+							<input type="email" class="" name="email" value="{{ old('email') }}">
 
-                    <div class="{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <label class="">E-Mail Address</label>
+							@if ($errors->has('email'))
+								<span class="">
+									<strong>{{ $errors->first('email') }}</strong>
+								</span>
+							@endif
+						</div>
+					</div>
+					<div class="{{ $errors->has('password') ? ' has-error' : '' }}">
+						<label class="">Password</label>
 
-                        <div class="">
-                            <input type="email" class="" name="email" value="{{ old('email') }}">
+						<div class="">
+							<input type="password" class="" name="password">
 
-                            @if ($errors->has('email'))
-                                <span class="">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                    </div>
+							@if ($errors->has('password'))
+								<span class="">
+									<strong>{{ $errors->first('password') }}</strong>
+								</span>
+							@endif
+						</div>
+					</div>
+					<div class="{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+						<label class="">Confirm Password</label>
 
-                    <div class="{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <label class="">Password</label>
+						<div class="">
+							<input type="password" class="" name="password_confirmation">
 
-                        <div class="">
-                            <input type="password" class="" name="password">
-
-                            @if ($errors->has('password'))
-                                <span class="">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                        <label class="">Confirm Password</label>
-
-                        <div class="">
-                            <input type="password" class="" name="password_confirmation">
-
-                            @if ($errors->has('password_confirmation'))
-                                <span class="">
-                                    <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="">
-                        <div class="">
-                            <button type="submit" class="">
-                                <i class=""></i>Register
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    @include('layouts.footer')
+							@if ($errors->has('password_confirmation'))
+								<span class="">
+									<strong>{{ $errors->first('password_confirmation') }}</strong>
+								</span>
+							@endif
+						</div>
+					</div>
+					<div class="">
+						<div class="">
+							<button type="submit" class="">
+								<i class=""></i>Register
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	@include('layouts.footer')
 </div>
+@endsection
+@section('footer')
+	@include('layouts.footer')
 @endsection
