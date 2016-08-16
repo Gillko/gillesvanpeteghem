e89fc78ac811ea106ae6ff42ce827271e66225c7
Gilles Vanpeteghem <gilles_vanpeteghem@hotmail.com>
2016-08-16 16:20:43 +0200

    62th Commit - changed layout files (new design - layout and footer)

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,27 +1,27 @@
-<footer>
-    <div class="row">
-        <div class="large-6 medium-6 small-12 columns">
-            <p class="text-left"><span class="blue">2016｜</span><span class="gray">Gilles Vanpeteghem</span></p>
-        </div>
-        <div class="large-6 medium-6 small-12 columns text-right socialicons">
-            <a href="https://www.facebook.com/vanpeteghemgilles" target="_blank">
-                <img src="../resources/assets/img/facebook.png" alt="https://www.facebook.com/vanpeteghemgilles">
-            </a>
-            <a href="https://be.linkedin.com/in/gilles-vanpeteghem" target="_blank">
-                  <img src="../resources/assets/img/linkedin.png" alt="https://be.linkedin.com/in/gilles-vanpeteghem">
-            </a>
-            <a href="https://twitter.com/Gilkko" target="_blank">
-                  <img src="../resources/assets/img/twitter.png" alt="https://twitter.com/Gilkko">
-            </a>
-            <a href="https://vimeo.com/gillesvanpeteghem" target="_blank">
-                  <img src="../resources/assets/img/vimeo.png" alt="https://vimeo.com/gillesvanpeteghem">
-            </a>
-            <a href="http://stackoverflow.com/users/2073428/gilko" target="_blank">
-                  <img src="../resources/assets/img/stackoverflow.png" alt="http://stackoverflow.com/users/2073428/gilko">
-            </a>
-            <a href="https://github.com/Gillko" target="_blank">
-                  <img src="../resources/assets/img/github.png" alt="https://github.com/Gillko">
-            </a>
-        </div>
-    </div>
+<footer class="footer">
+	<div class="row">
+		<div class="large-6 medium-6 small-12 columns">
+			<p class="text-left"><span class="blue">2016｜</span><span class="gray">Gilles Vanpeteghem</span></p>
+		</div>
+		<div class="large-6 medium-6 small-12 columns text-right socialicons">
+			<a href="https://www.facebook.com/vanpeteghemgilles" target="_blank">
+				<img src="{{ url('../resources/assets/img/facebook.png') }}" alt="https://www.facebook.com/vanpeteghemgilles">
+			</a>
+			<a href="https://be.linkedin.com/in/gilles-vanpeteghem" target="_blank">
+				<img src="{{ url('../resources/assets/img/linkedin.png') }}" alt="https://be.linkedin.com/in/gilles-vanpeteghem">
+			</a>
+			<a href="https://twitter.com/Gilkko" target="_blank">
+				<img src="{{ url('../resources/assets/img/twitter.png') }}" alt="https://twitter.com/Gilkko">
+			</a>
+			<a href="https://vimeo.com/gillesvanpeteghem" target="_blank">
+				<img src="{{ url('../resources/assets/img/vimeo.png') }}" alt="https://vimeo.com/gillesvanpeteghem">
+			</a>
+			<a href="http://stackoverflow.com/users/2073428/gilko" target="_blank">
+				<img src="{{ url('../resources/assets/img/stackoverflow.png') }}" alt="http://stackoverflow.com/users/2073428/gilko">
+			</a>
+			<a href="https://github.com/Gillko" target="_blank">
+				<img src="{{ url('../resources/assets/img/github.png') }}" alt="https://github.com/Gillko">
+			</a>
+		</div>
+	</div>
 </footer>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,86 +1,93 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Gilles Vanpeteghem</title>
-
-        <link rel="shortcut icon" href="../resources/assets/img/favicon.ico" type="image/x-icon">
-        <link rel="icon" href="../resources/assets/img/favicon.ico" type="image/x-icon">
-
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Gilles Vanpeteghem</title>
+		<link rel="shortcut icon" href="{{ url('../resources/assets/img/favicon.ico') }}" type="image/x-icon">
+        <link rel="icon" href="{{ url('../resources/assets/img/favicon.ico') }}" type="image/x-icon">
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <link href="../resources/assets/css/libraries/foundation/foundation.min.css" rel="stylesheet" type="text/css">
-
-        <link href="../resources/assets/css/app.css" rel="stylesheet" type="text/css">
-
+        <link href="{{ url('../resources/assets/css/libraries/foundation/foundation.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ url('../resources/assets/css/app.css') }}" rel="stylesheet" type="text/css">
         <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
-    </head>
-    <body>
-        <!-- Responsive navigation -->
-        <div class="title-bar title-bar-center title-bar-background" data-responsive-toggle="example-menu" data-hide-for="medium">
-            <div class="title-bar-title">
-                <a href="/">
-                    <h1 class="logo">GILVAN</h1>
-                </a>
-            </div>
-            <button class="menu-icon menu-icon-gv" type="button" data-toggle></button>
-        </div>
-      
-        <div class="top-bar-background">
-            <div class="row">
-                <div class="large-12">
-                    <div class="top-bar top-bar-background" id="example-menu">
-                        <div class="top-bar-left">
-                        <a href="/">
-                            <h1 class="logo">GILVAN</h1>
-                            </a>
-                          </div>
-                          <div class="top-bar-right top-bar-background">
-                            <ul class="dropdown menu menu-bottom" data-dropdown-menu>
-                                @if (Auth::guest())
-                                    <li>
-                                        <a href="#expertise">expertise</a>
-                                    </li>
-                                    <li>
-                                        <a href="#skills">skills</a>
-                                    </li>
-                                    <li>
-                                        <a href="#about">about</a>
-                                    </li>
-                                @else
-                                    <li>
-                                        <a href="#expertise">expertise</a>
-                                    </li>
-                                    <li>
-                                        <a href="#skills">skills</a>
-                                    </li>
-                                    <li>
-                                        <a href="#about">about</a>
-                                    </li>
-                                    <li class="">
-                                        <a href="#" class="" data-toggle="dropdown" role="button" aria-expanded="false">
-                                            {{ Auth::user()->name }} <span class=""></span>
-                                        </a>
+	</head>
+	<body>
+		<div class="wrapper">
+			<!-- Responsive navigation -->
+			<div class="title-bar title-bar-center title-bar-background" data-responsive-toggle="example-menu" data-hide-for="medium">
+				<div class="title-bar-title">
+					<a href="/">
+						<h1 class="logo">GILVAN</h1>
+					</a>
+				</div>
+				<button class="menu-icon menu-icon-gv" type="button" data-toggle></button>
+			</div>
+			<div class="top-bar-background">
+				<div class="row">
+					<div class="large-12">
+						<div class="top-bar top-bar-background" id="example-menu">
+							<div class="top-bar-left">
+							<a href="/">
+								<h1 class="logo">GILVAN</h1>
+								</a>
+							  </div>
+							  <div class="top-bar-right top-bar-background">
+								<ul class="dropdown menu menu-bottom" data-dropdown-menu>
+									@if (Auth::guest())
+										<li>
+	                                        <a href="{{ url('/#expertise') }}">expertise</a>
+	                                    </li>
+	                                    <li>
+	                                        <a href="{{ url('/#skills') }}">skills</a>
+	                                    </li>
+	                                    <li>
+	                                        <a href="{{ url('/#about') }}">about</a>
+	                                    </li>
+									@else
+										<li>
+	                                        <a href="{{ url('/#expertise') }}">expertise</a>
+	                                    </li>
+	                                    <li>
+	                                        <a href="{{ url('/#skills') }}">skills</a>
+	                                    </li>
+	                                    <li>
+	                                        <a href="{{ url('/#about') }}">about</a>
+	                                    </li>
+										<li class="">
+											<a href="#" class="" data-toggle="dropdown" role="button" aria-expanded="false">
+												{{ Auth::user()->name }} <span class=""></span>
+											</a>
 
-                                        <ul class="dropdown menu submenu-background-gv" data-dropdown-menu>
-                                            <li><a href="{{ url('/logout') }}">Logout</a></li>
-                                        </ul>
-                                    </li>
-                                @endif
-                            </ul>
-                          </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        @yield('content')
-
+											<ul class="dropdown menu submenu-background-gv" data-dropdown-menu>
+												<li>
+													<a href="{{ url('/bookmarks') }}">bookmarks</a>
+												</li>
+												<li>
+													<a href="{{ url('/categories') }}">categories</a>
+												</li>
+												<li>
+													<a href="{{ url('/types') }}">types</a>
+												</li>
+												<li>
+													<a href="{{ url('/logout') }}">Logout</a>
+												</li>
+											</ul>
+										</li>
+									@endif
+								</ul>
+							  </div>
+						</div>
+					</div>
+				</div>
+			</div>
+			@yield('content')
+		</div>
+		<div class="push"></div>
     </div>
-        <script src="../resources/assets/js/libraries/jquery/jquery-1.12.1.min.js"></script>
-        <script src="../resources/assets/js/libraries/foundation/foundation.min.js"></script>
-        <script src="../resources/assets/js/libraries/foundation/app.js"></script>
-        <script src="../resources/assets/js/app.js"></script>
+    @yield('footer')
+	<script src="{{ url('../resources/assets/js/libraries/jquery/jquery-1.12.1.min.js') }}"></script>
+    <script src="{{ url('../resources/assets/js/libraries/foundation/foundation.min.js') }}"></script>
+    <script src="{{ url('../resources/assets/js/libraries/foundation/app.js') }}"></script>
+    <script src="{{ url('../resources/assets/js/app.js') }}"></script>
 </body>
 </html>
